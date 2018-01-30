4982045c8e35d43f4039bb1e4d9018fe5513358c
wangchy <wangchy51@163.com>
2018-01-30 18:20:28 +0800

    123123

---
+++ src/main/java/TestRain/TestServlet.java
@@ -0,0 +1,44 @@
+package TestRain;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		int int_a = Integer.parseInt(request.getParameter("a"));
+		int int_b = Integer.parseInt(request.getParameter("b"));
+		int sum =int_a+int_b;
+		String result = Integer.toString(sum);
+
+		response.getWriter().append(result);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
